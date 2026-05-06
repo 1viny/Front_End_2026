7480f9142b5c7ec9332f1c127b3e7b1f3eded21a
vinicyus.faria <108416319+1viny@users.noreply.github.com>
2026-05-06 09:23:21 -0300

    Delete botao directory

--- botao/script.js
+++
@@ -1,9 +0,0 @@
- let cliques = 0;
-
-    const botao = document.getElementById("botao");
-    const contador = document.getElementById("contador");
-
-    botao.addEventListener("click", function() {
-      cliques++;
-      contador.innerText = cliques;
-    });
